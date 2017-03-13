00f205bca1e96451958fc2ed4be93ba240c658e3
gayson <playagayson@gmail.com>
2017-03-13 16:58:39 +0800

    3/13 16:58

--- hooks/views.py
+++ hooks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+import hmac, hashlib
 import commands
 
 pathlist=[
@@ -12,17 +13,24 @@ bashlist=[
 '~/github/Webhooks/bashfile/pybash',
 ]
 
+secret = 'WebHooks'
+
 def updateRepo(request,index):
 	pos=int(index)
 	if(pos<0 or pos>=len(pathlist)):
 		return HttpResponse("false:\ninvalid index.")
 
 	try:
-		print(request.META)
-		print("********************")
-		print(request.body)
-		signature=request.META.get('HTTP_X_Hub_Signature')
-		return HttpResponse("test header")
+		signature=request.META.get('HTTP_X_HUB_SIGNATURE')
+		signlist=signature.split(str='=')
+		algo=signlist[0]
+		key=signlist[1]
+
+		encode=''
+		if algo=='sha1':
+			encode=hmac.new(secret,request.body,digestmod=hashlib.sha1)
+		if key==encode:
+			return HttpResponse("success")
 	except:
 		return HttpResponse("test error")
 
